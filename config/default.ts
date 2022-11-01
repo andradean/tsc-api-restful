617d5f66fe0dfb95744eb69d23163f02ef01a1dd
andradean <viniciusansilva@outlook.com>
2022-10-31 23:21:09 -0300

    proteção de dados

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,8 @@
+const dbUser = process.env.DB_USER
+const dbPass =  process.env.DB_PASS 
+
 export default {
     port: 8000,
-    dbUri: "mongodb+srv://root:Senha8656@cluster0.ehrm82v.mongodb.net/?retryWrites=true&w=majority"
+    dbUri: `mongodb+srv://${dbUser}:${dbPass}@cluster0.ehrm82v.mongodb.net/?retryWrites=true&w=majority`
 
 }
