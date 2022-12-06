ab5a8052b933dac9a12783a2b51b54b320438798
andradean <viniciusansilva@outlook.com>
2022-12-05 23:37:25 -0300

    tratando erros com logger

--- src/controller/movie.controller.ts
+++ src/controller/movie.controller.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
+import { Logger } from "winston";
 import logger from "../../config/logger";
 import { movieModel } from "../model/movie";
 
 export async function createMovie(req:Request, res:Response) {
-    res.status(200).send("Filme criado com sucesso")
+    try {
+        const data = req.body
+        const movie = movieModel.create(data)
+        res.status(200).send(movie)
+
+        
+    } catch (e: any) {
+        logger.error(`erro no sistema ${e.message}`)
+        
+        
+         }
+    
 }
